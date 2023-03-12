4fce32a8a689d3d42907006598bba1e7b7026bcd
Tina Mladenović <126920653+tinamladenovic@users.noreply.github.com>
2023-03-12 23:46:17 +0100

    Add files via upload

---
+++ FiveStarTours-Project/FiveStarTours/Model/Vehicle.cs
@@ -0,0 +1,61 @@
+﻿using FiveStarTours.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace FiveStarTours.Model
+{
+    public class Vehicle : ISerializable
+    {
+        public int Id { get; set; }
+        public int IdLocation { get; set; }
+        public Location Location { get; set; } //lokacija
+        public int MaxPersonNum { get; set; } // maksimalni broj osoba
+        public int IdLanguage { get; set; }
+        public Language Language { get; set; } //jezik
+        public List<string> ImageUrls { get; set; } // slike
+
+
+        public Vehicle() { }
+
+        public Vehicle(int id, int idLocation, Location location, int maxPersonNum, int idLanguage, Language language, List<string> imageUrls)
+        {
+            Id = id;
+            IdLocation = idLocation;
+            Location = location;
+            MaxPersonNum = maxPersonNum;
+            IdLanguage = idLanguage;
+            Language = language;
+            ImageUrls = imageUrls;
+        }
+
+
+        public string[] ToCSV()
+        {
+            string[] csvValues =
+            {
+                Id.ToString(),
+                IdLocation.ToString(),
+                MaxPersonNum.ToString(),
+                string.Join(';', IdLanguage),
+                string.Join(';', ImageUrls)
+            };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            IdLocation = Convert.ToInt32(values[1]);
+            MaxPersonNum = Convert.ToInt32(values[2]);
+            IdLanguage = Convert.ToInt32(values[3]);
+            ImageUrls = values[4].Split(';').ToList();
+        }
+
+        
+    }
+}  
