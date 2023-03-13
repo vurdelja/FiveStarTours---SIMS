3775121844f6910ac1e5ecb19a11fa26eb64734e
Sonja Sekulic <sonjasekulic434@gmail.com>
2023-03-14 00:24:02 +0100

    Added ToursRepository

---
+++ FiveStarTours-Project/FiveStarTours/Repository/ToursRepository.cs
@@ -0,0 +1,70 @@
+﻿// Modeled on CommentRepository from InitialProject
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FiveStarTours.Serializer;
+using FiveStarTours.Model;
+
+namespace FiveStarTours.Repository
+{
+    public class ToursRepository
+    {
+        private const string FilePath = "../../../Resources/Data/tours.csv";
+
+        private readonly Serializer<Tour> _serializer;
+
+        private List<Tour> _tours;
+
+        public ToursRepository()
+        {
+            _serializer = new Serializer<Tour>();
+            _tours = _serializer.FromCSV(FilePath);
+        }
+
+        public List<Tour> GetAll()
+        {
+            return _serializer.FromCSV(FilePath);
+        }
+
+        public Tour Save(Tour tour)
+        {
+            tour.Id = NextId();
+            _tours = _serializer.FromCSV(FilePath);
+            _tours.Add(tour);
+            _serializer.ToCSV(FilePath, _tours);
+            return tour;
+        }
+
+        public int NextId()
+        {
+            _tours = _serializer.FromCSV(FilePath);
+            if (_tours.Count < 1)
+            {
+                return 1;
+            }
+            return _tours.Max(t => t.Id) + 1;
+        }
+
+        /*public void Delete(Tour tour)
+        {
+            _tours = _serializer.FromCSV(FilePath);
+            Tour founded = _tours.Find(t => t.Id == tour.Id);
+            _tours.Remove(founded);
+            _serializer.ToCSV(FilePath, _tours);
+        }*/
+
+        /*public Tour Update(Tour tour)
+        {
+            _tours = _serializer.FromCSV(FilePath);
+            Tour current = _tours.Find(t => t.Id == tour.Id);
+            int index = _tours.IndexOf(current);
+            _tours.Remove(current);
+            _tours.Insert(index, tour);       // keep ascending order of ids in file 
+            _serializer.ToCSV(FilePath, _tours);
+            return tour;
+        }*/
+    }
+}
