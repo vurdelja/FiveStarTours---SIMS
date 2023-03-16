a02d6cece1f276f41e87ae8a71d97fa70765961e
Sonja Sekulic <sonjasekulic434@gmail.com>
2023-03-16 02:07:26 +0100

    Added creation of new tour.

--- FiveStarTours-Project/FiveStarTours/Repository/ToursRepository.cs
+++ FiveStarTours-Project/FiveStarTours/Repository/ToursRepository.cs
@@ -47,24 +47,5 @@ namespace FiveStarTours.Repository
             }
             return _tours.Max(t => t.Id) + 1;
         }
-
-        /*public void Delete(Tour tour)
-        {
-            _tours = _serializer.FromCSV(FilePath);
-            Tour founded = _tours.Find(t => t.Id == tour.Id);
-            _tours.Remove(founded);
-            _serializer.ToCSV(FilePath, _tours);
-        }*/
-
-        /*public Tour Update(Tour tour)
-        {
-            _tours = _serializer.FromCSV(FilePath);
-            Tour current = _tours.Find(t => t.Id == tour.Id);
-            int index = _tours.IndexOf(current);
-            _tours.Remove(current);
-            _tours.Insert(index, tour);       // keep ascending order of ids in file 
-            _serializer.ToCSV(FilePath, _tours);
-            return tour;
-        }*/
     }
 }
