39682409bbc89f1fe3234aa57f3994f8a569dd5d
Olja <olgaponjevic@gmail.com>
2023-03-17 23:25:42 +0100

    ToursListingView

--- FiveStarTours-Project/FiveStarTours/View/ToursListingView.xaml.cs
+++ FiveStarTours-Project/FiveStarTours/View/ToursListingView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,17 +12,31 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+using FiveStarTours.Model;
+using FiveStarTours.Repository;
 
 namespace FiveStarTours.View
 {
     /// <summary>
     /// Interaction logic for ToursListingView.xaml
     /// </summary>
+    /// 
+
     public partial class ToursListingView : Window
-    {
+    { 
+        public static ObservableCollection<Tour> Tours { get; set; }
+        public Tour SelectedTour{ get; set; }
+
+
         public ToursListingView()
         {
-            InitializeComponent();
+            
+        }
+
+        private void TourSearchClick_Checked(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
