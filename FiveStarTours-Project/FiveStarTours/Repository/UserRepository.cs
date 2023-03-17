a823d3cbf412871a50a4ef2b409ab3d85e80e039
Tina Mladenović <126920653+tinamladenovic@users.noreply.github.com>
2023-03-17 20:12:32 +0100

    Update UserRepository.cs

--- FiveStarTours-Project/FiveStarTours/Repository/UserRepository.cs
+++ FiveStarTours-Project/FiveStarTours/Repository/UserRepository.cs
@@ -10,7 +10,7 @@ namespace FiveStarTours.Repository
 {
     public class UserRepository
     {
-        private const string FilePath = "../../../Resources/Data/users.txt";
+        private const string FilePath = "../../../Resources/Data/users.csv";
 
         private readonly Serializer<User> _serializer;
 
