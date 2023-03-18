c24288a6ca8c08b6416a29269b6e1d9d16561d02
Olja <olgaponjevic@gmail.com>
2023-03-18 14:13:09 +0100

    commit

--- FiveStarTours-Project/FiveStarTours/View/ToursListingView.xaml.cs
+++ FiveStarTours-Project/FiveStarTours/View/ToursListingView.xaml.cs
@@ -34,6 +34,8 @@ namespace FiveStarTours.View
         public ToursListingView()
         {
             InitializeComponent();
+            DataContext = this;
+
             //_repository = new TourRepository();
 
 
