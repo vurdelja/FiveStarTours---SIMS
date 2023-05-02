c231f3d3152174dcd078c86d7c452212ffec5476
Sonja Sekulic <sonjasekulic434@gmail.com>
2023-05-02 18:59:15 +0200

    Added view model for tour registartion form.

---
+++ FiveStarTours-Project/FiveStarTours/ViewModel/ICommandImplementation/Command.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FiveStarTours.ViewModel.ICommandImplementation
+{
+    public class Command : ICommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public Command(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        public Command(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute?.Invoke() ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute.Invoke();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
